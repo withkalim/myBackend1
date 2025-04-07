e38c0635baa964763c2e5c3f0612c9cca44ea5ae
withkalim <142097334+withkalim@users.noreply.github.com>
2025-04-07 20:14:21 +0530

    updates

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,6 @@
-export const Register = (req, res) =>{
+import User from "../model/user.schema.js";
+
+export const Register = async (req, res) =>{
     try {
         const { name, email, password, conformPassword } = req.body
         console.log(name, email, password, conformPassword);
@@ -9,6 +11,24 @@ export const Register = (req, res) =>{
         if (password !== conformPassword) {
           return res.send("Please enter correct password");
         }
+        const isEmailExist = await User.find({ email: email});
+        // const isEmailExist = await User.deleteOne({ email: email});
+
+        console.log(isEmailExist, "is email exist");
+        if(isEmailExist?.length > 0){
+          return res.send("Email already taken, please use another one");
+        }
+
+        // mongoDB save data 
+       const newUser = User({
+        name,
+        email,
+        password
+       });
+
+       const responsefromDB = await newUser.save();
+       console.log(newUser, "recent newUSer");
+
         return res.send("Horrey Register Successfull");
       } catch (error) {
         console.error(error);
