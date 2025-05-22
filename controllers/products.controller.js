911230d23e1103f74c85d7ee4975cb1172413133
withkalim <142097334+withkalim@users.noreply.github.com>
2025-05-22 12:51:56 +0530

    update

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -19,7 +19,7 @@ export const AddProduct = async (req, res) => {
     const isUserExist = await User.findById(userId);
     console.log(isUserExist, " isUserExist");
     if (!isUserExist) {
-     return res.json({ success: false, message: "User not found" });
+      return res.json({ success: false, message: "User not found" });
     }
 
     if (isUserExist.role != "seller") {
@@ -40,9 +40,33 @@ export const AddProduct = async (req, res) => {
     await newProduct.save();
     console.log(" This all are newProduct data ", newProduct);
 
-    return res.json({ success: true, message: "Product successfully created " });
+    return res.json({
+      success: true,
+      message: "Product successfully created ",
+    });
   } catch (error) {
     console.log(error, "someting error in products controller");
+    return res.json({ success: false, message: error });
+  }
+};
+
+
+export const AddedProduct = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    if (!userId) {
+      return res.json({ success: false, message: "User id is required" });
+    }
+    const products = await Product.find({ userId: userId });
+    console.log(products, "products");
+
+    return res.json({
+      products: products,
+      success: true,
+      message: "Product successfully fetched",
+    });
+  } catch (error) {
+       console.log(error, "someting error in Addedproducts fetching");
     return res.json({ success: false, message: error });
   }
 };
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { AddProduct } from "../controllers/products.controller.js";
+import { AddProduct, AddedProduct} from "../controllers/products.controller.js";
 
 
 const productRoutes = Router();
 
 productRoutes.post("/product-add", AddProduct);
+productRoutes.post("/added-product", AddedProduct);
+
 
 export default productRoutes;
